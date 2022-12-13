1a90c10b19fb181f5f01229309958d799043ac58
finfire@gmx.de <finfire@gmx.de>
2022-12-13 09:51:33 +0100

    android app neu erzeugt

--- apps/bieralarm/src/app/app.module.ts
+++ apps/bieralarm/src/app/app.module.ts
@@ -10,7 +10,12 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    //firebase.initializeApp(environment.firebase),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
